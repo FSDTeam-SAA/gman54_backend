24dcccb5a77ae75fd2396100f214b043ea5c31c9
Tahsin <gazitahsin323@gmail.com>
2025-05-31 16:28:14 +0600

    `Removed user route and replaced with auth route in mainroute/index.js and removed user.js file`

---
+++ route/auth.route.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { changePassword, forgetPassword, login, logout, refreshToken, register, resetPassword, UserData, verifyEmail } from '../controller/auth.controller'
+import { protect } from '../middleware/authMiddleware'
+
+const router = express.Router()
+
+router.post('/register', register),
+router.post('/login', login),
+router.get("/user-data", protect, UserData)
+router.post('/verify', verifyEmail),
+router.post('/forget', forgetPassword),
+router.post('/reset-password', resetPassword)
+router.post('/change-password', protect, changePassword)
+router.post('/refresh-token', refreshToken)
+router.post ('/logout',protect, logout)
+
+
+export default router
