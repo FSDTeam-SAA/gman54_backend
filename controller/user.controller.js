e4146d846ab9b000f2269999fe2a921cb45b8c0c
NurMohammad56 <milonsardar56@gmail.com>
2025-06-01 15:25:35 +0600

    Implement user profile management: add getProfile and updateProfile functions, create user and farmCategory models, and set up user routes

---
+++ controller/user.controller.js
@@ -0,0 +1,59 @@
+import catchAsync from "../utils/catchAsync";
+import sendResponse from "../utils/sendResponse";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import AppError from "../errors/AppError";
+import httpStatus from "http-status";
+import { User } from "../models";
+
+// Get user profile
+export const getProfile = catchAsync(async (req, res) => {
+  const user = await User.findById(req.user._id).select(
+    "-password -refreshToken -verificationInfo -password_reset_token"
+  );
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Profile fetched successfully",
+    data: user,
+  });
+});
+
+// Update user profile
+export const updateProfile = catchAsync(async (req, res) => {
+  const { name, username, phone, street, city, state, zipCode } = req.body;
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  if (name) user.name = name;
+  if (username) user.username = username;
+  if (phone) user.phone = phone;
+  if (street) user.address.street = street;
+  if (city) user.address.city = city;
+  if (state) user.address.state = state;
+  if (zipCode) user.address.zipCode = zipCode;
+
+  if (req.file) {
+    const result = await uploadOnCloudinary(req.file.buffer, {
+      folder: "avatars",
+    });
+    user.avatar = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  }
+
+  await user.save();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Profile updated successfully",
+    data: user,
+  });
+});
