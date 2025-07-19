58a216bb4feacfcb8892a42e494651bee70dd463
Tahsin <gazitahsin323@gmail.com>
2025-07-19 16:07:51 +0600

    `Update server.js: change express.json limit to 10mb and add express.urlencoded with 10mb limit`

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
 // Mount the main router
@@ -45,7 +46,6 @@ app.use("/api/v1", router);
 app.get("/", (req, res) => {
   res.send("Server is running...!!");
 });
-
 app.use(globalErrorHandler);
 app.use(notFound);
 
